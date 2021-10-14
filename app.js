40ef41058349d1c6b886c5e8ea9dbd79e18a4952
victordurco <victordurco@gmail.com>
2021-10-14 14:32:42 -0300

    node connected to the database

--- app.js
+++ app.js
@@ -1,8 +1,27 @@
 import express from "express";
 import cors from "cors";
+import pg from "pg";
+
+const { Pool } = pg;
+
+const connection = new Pool({
+  user: "bootcamp_role",
+  password: "senha_super_hiper_ultra_secreta_do_role_do_bootcamp",
+  host: "localhost",
+  port: 5432,
+  database: "boardcamp",
+});
 
 const app = express(); // create server
 app.use(express.json());
 app.use(cors());
+
+app.get("/", (req, res) => {
+  const query = connection.query("SELECT * FROM categories");
+  query.then((result) => {
+    console.log(result.rows);
+  });
+  res.send("hello server");
+});
 
 app.listen(4000); // start server
