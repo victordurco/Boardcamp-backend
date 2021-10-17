25f5bd5d25043f62ee588d911f13b936e878615c
victordurco <victordurco@gmail.com>
2021-10-17 19:10:19 -0300

    post game done

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import pg from "pg";
+import joi from "joi";
 
 const { Pool } = pg;
 
@@ -18,8 +19,28 @@ const inputIsEmpty = (input) => {
 };
 
 const nameIsNotAvailable = async (name, table) => {
-  const categories = await connection.query(`SELECT * FROM ${table}`);
-  return categories.rows.some((elem) => elem.name === name);
+  const query = await connection.query(`SELECT * FROM ${table}`);
+  return query.rows.some((elem) => elem.name === name);
+};
+
+const gameObjIsInvalid = async (game) => {
+  let categoryIsValid = false;
+  const category = parseInt(game.categoryId);
+  const gameSchema = joi.object({
+    name: joi.string().min(3).required(),
+    image: joi.string().uri(),
+    categoryId: joi.number().integer().min(0),
+    stockTotal: joi.number().integer().min(0),
+    pricePerDay: joi.number().integer().min(0),
+  });
+  const query = await connection.query("SELECT * FROM categories");
+  query.rows.forEach((elem) => {
+    if (elem.id === category) categoryIsValid = true;
+  });
+  const { error } = gameSchema.validate(game);
+  console.log(error, !categoryIsValid);
+  if (error || !categoryIsValid) return true;
+  else return false;
 };
 
 const app = express(); // create server
@@ -85,6 +106,35 @@ app.get("/games", async (req, res) => {
     }
   } catch {
     res.sendStatus(500);
+  }
+});
+
+app.post("/games", async (req, res) => {
+  try {
+    const game = req.body;
+    if (await gameObjIsInvalid(game)) {
+      res.sendStatus(400);
+    } else if (await nameIsNotAvailable(game.name, "games")) {
+      res.sendStatus(409);
+    } else {
+      const query = await connection.query(
+        `INSERT INTO 
+				    games (name, image, "stockTotal", "categoryId", "pricePerDay") 
+			    VALUES 
+				    ($1, $2, $3, $4, $5);`,
+        [
+          game.name,
+          game.image,
+          game.stockTotal,
+          game.categoryId,
+          game.pricePerDay,
+        ]
+      );
+      res.sendStatus(201);
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("é aqui");
   }
 });
 
