c01505b6b61bd0de84e669a7b55cc44a05f12aba
victordurco <victordurco@gmail.com>
2021-10-18 01:48:32 -0300

    get rentals obj fixed

--- app.js
+++ app.js
@@ -311,7 +311,31 @@ app.get("/rentals", async (req, res) => {
         ${customerId ? `WHERE rentals."customerId" = '${customerId}'` : ""}
         ${gameId ? `WHERE rentals."gameId" = '${gameId}'` : ""}
     `);
-      res.status(200).send(query.rows);
+      const rentals = query.rows.map((rental) => {
+        return {
+          id: rental.id,
+          customerId: rental.customerId,
+          gameId: rental.gameId,
+          rentDate: dayjs(rental.rentDate).format("YYYY-MM-DD"),
+          daysRented: rental.daysRented,
+          returnDate: rental.returnDate
+            ? dayjs(rental.returnDate).format("YYYY-MM-DD")
+            : null,
+          originalPrice: rental.originalPrice,
+          delayFee: rental.delayFee,
+          customer: {
+            id: rental.customerId,
+            name: rental.customerName,
+          },
+          game: {
+            id: rental.gameId,
+            name: rental.gameName,
+            categoryId: rental.categoryId,
+            categoryName: rental.categoryName,
+          },
+        };
+      });
+      res.status(200).send(rentals);
     }
   } catch {
     res.sendStatus(500);
